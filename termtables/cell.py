15456a8160435f673591bcddf60f2a87703da4af
Matthew Lemon <lemon@matthewlemon.com>
2018-07-14 20:09:37 +0100

    started table render testing

--- termtables/cell.py
+++ termtables/cell.py
@@ -1,2 +1,43 @@
+from typing import List, Tuple
+
+
+def _pad(string: str, left_pad=0, right_pad=0) -> str:
+    output = string
+    pad_char = " "
+    if left_pad > 0:
+        output = (pad_char * left_pad) + output
+    if right_pad > 0:
+        output = output + (pad_char * right_pad)
+    return output
+
+
+class Row:
+    def __init__(self, grid: bool=True) -> None:
+        self.grid = grid
+        self._cell_list: List[Cell] = []
+
+    def add_row(self, lst: List[Tuple[str, int, int]]) -> None:
+        for x in lst:
+            self._cell_list.append(Cell(*lst[0]))
+
+    def render(self):
+        output = []
+        if self.grid:
+            for c in self._cell_list:
+                output.append("|")
+                output.append(c._render())
+        return "".join(output)
+
+
 class Cell:
-    pass
+    """A single cell."""
+
+    def __init__(self, string, left_pad, right_pad) -> None:
+        self.string = string
+        self.left_pad = left_pad
+        self.right_pad = right_pad
+
+    def _render(self):
+        self.output = _pad(self.string, self.left_pad, self.right_pad)
+        return self.output
+
--- tests/test_termtables.py
+++ tests/test_termtables.py
@@ -1,8 +1,31 @@
 import pytest
 
-from termtables.cell import Cell
+from termtables.cell import _pad, Cell, Row
 
 
-def test_cell():
-    c = Cell()
-    assert c
+def test_cell_padding_single():
+    s = "Data String"
+    assert _pad(s, 1, 1) == " Data String "
+
+
+def test_cell_padding_double():
+    s = "Data String"
+    assert _pad(s, 2, 2) == "  Data String  "
+
+
+def test_cell_padding_long_right():
+    s = "Data String"
+    assert _pad(s, 2, 10) == "  Data String          "
+
+
+def test_cell_render():
+    c = Cell("Data String", 1, 1)
+    assert c._render() == " Data String "
+
+
+def test_row_render():
+    r = Row(grid=True)
+    r.add_row([
+        ("Data String", 1, 1),
+    ])
+    assert r.render() == "| Data String "
