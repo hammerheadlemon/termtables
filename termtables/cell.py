17587fc6eb975f6df048ef51c14513942bac1628
Matthew Lemon <lemon@matthewlemon.com>
2018-07-16 17:11:54 +0100

    working on row calculating code

--- termtables/cell.py
+++ termtables/cell.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
+from collections import OrderedDict
 
 
 def _pad(string: str, left_pad=0, right_pad=0) -> str:
@@ -15,10 +16,21 @@ class Row:
     def __init__(self, grid: bool=True) -> None:
         self.grid = grid
         self._cell_list: List[Cell] = []
+        self._marker_cell_length = 0 # the longest cell in header or row
 
-    def add_row(self, lst: List[Tuple[str, int, int]]) -> None:
+    def _add_row(self, lst: List[Tuple[str, int, int]]) -> None:
         for x in lst:
             self._cell_list.append(Cell(*lst[0]))
+
+    def add_row(self, row: List[str]) -> None:
+        self._update_marker_cell_length(row)
+
+    def add_header(self, row: List[str]) -> None:
+        self._update_marker_cell_length(row)
+
+    def _update_marker_cell_length(self, lst: List[str]) -> None:
+        """Updates self._marker_cell attribute"""
+        self._marker_cell_length = len(sorted(lst)[-1])
 
     def render(self):
         output = []
--- tests/test_termtables.py
+++ tests/test_termtables.py
@@ -25,7 +25,20 @@ def test_cell_render():
 
 def test_row_render():
     r = Row(grid=True)
-    r.add_row([
+    r._add_row([
         ("Data String", 1, 1),
     ])
     assert r.render() == "| Data String "
+
+
+def test_row_render_long_cell():
+    """Padding for cells based on longest cell content."""
+    r = Row(grid=True)
+    r.add_header(["Data 1", "Data 2"])
+    r.add_row([
+        ("Data String", "Bigger Data String"),
+    ])
+    assert r.render() == """| Data 1      | Data 2
+                            -----------------------------------
+                            | Data String | Bigger Data String
+                         """
